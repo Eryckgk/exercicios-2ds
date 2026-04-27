228d6898f0362c42f312de70fc94d1b2a35c4b6c
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2026-04-27 23:57:32 +0000

    Concluido

--- 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
+++ 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
@@ -1,11 +1,14 @@
 export default function App() {
   const favoritaMateria = "Programação Web"
+  const filmeFavorito = "Interestelar"
 
   return(
     <div>
       <h1>Oi meu nome é Eryck Fernando</h1>
 
-      <p>Minha materia favorita é: ${favoritaMateria}</p>
+      <p>Minha matéria favorita é: {favoritaMateria}</p>
+
+      <p>Meu filme favorito é: {filmeFavorito}</p>
     </div>
   )
 }
